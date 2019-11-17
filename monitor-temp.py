1609573f3f0d48377a724379bee9520ff61d63cd
Tiger Nie <nhl0819@gmail.com>
2019-11-16 22:57:06 -0600

    Script for sending pi_temp data to server

--- monitor-temp.py
+++ monitor-temp.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import os
 import time
+import requests
+
+
+url = "https://home.tigernie.com/home_api/"
 
 def measure_temp():
     temp = float(os.popen("vcgencmd measure_temp").readline().strip().replace("temp=","").split("'")[0])
@@ -8,6 +12,19 @@ def measure_temp():
 
 
 def send_temp():
+    name = os.popen("hostname").readline().strip()
+    temp = measure_temp()
+    res = requests.post(url+"pi_temp",
+                        data={
+                            "name": name,
+                            "temperature": temp
+                        }
+                        )
+    print(res)
+    print(res.json())
+
+
+def print_temp():
     name = os.popen("hostname").readline().strip()
     temp = measure_temp()
     print(name)
