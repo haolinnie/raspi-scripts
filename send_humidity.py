3696e67d02a12d9f9e9b6f664a9aafeeb7097be9
Tiger Nie <nhl0819@gmail.com>
2019-12-08 14:50:59 -0600

    Added script to POST humidity

---
+++ send_humidity.py
@@ -0,0 +1,25 @@
+import adafruit_dht
+import board
+
+import sys
+import requests
+
+dht_device = adafruit_dht.DHT11(board.D17)
+temperature = dht_device.temperature
+humidity = dht_device.humidity
+
+url = "https://home.tigernie.com/home_api/sensor_humidity"
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        res = requests.post(url, data={
+                                     "name": "laundry closet",
+                                     "humidity": humidity,
+                                    })
+        print(res)
+        print(res.json())
+    else:
+        print("Temperature: ", temperature)
+        print("Humidity: ", humidity)
+
